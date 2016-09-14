add403e7c80c322be872c8d1e107a4c489727f6b
thejohnhoffer <hoffexi@gmail.com>
2016-09-14 10:31:38 -0400

    structure

--- bfly/stack.js
+++ bfly/stack.js
@@ -4,25 +4,21 @@
 //
 //-----------------------------------
 
-DOJO.Stack = function() {
-
+DOJO.Stack = function(src_terms){
+    // For all sources
+    DOJO.Source(src_terms);
+    // Overlay the two layers
+    for (i in this.terms) {
+        // for all the depths
+        for (var z in new Uint8Array(this.depth)) {
+            var source = new DOJO.Source(this.src[i%2]);
+            this.share(source, this.terms[i]);
+            this.layers.push(source);
+        }
+    }
 }
 
 DOJO.Stack.prototype = {
-
-    init: function(src_terms){
-        // For all sources
-        DOJO.Source(src_terms);
-        // Overlay the two layers
-        for (i in this.terms) {
-            // for all the depths
-            for (var z in new Uint8Array(this.depth)) {
-                var source = new DOJO.Source(this.src[i%2]);
-                this.share(source, this.terms[i]);
-                this.layers.push(source);
-            }
-        }
-    },
     share: DOJO.Source().share,
     src: [{},{segmentation: true}],
     terms: [{},{opacity: .4}],
