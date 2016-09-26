2e29a64ce185775bf5cb414e39052530e16b4fe1
thejohnhoffer <hoffexi@gmail.com>
2016-09-26 16:00:48 -0400

    remove need for openSeadragonGL

--- bfly/link.js
+++ bfly/link.js
@@ -1,7 +1,7 @@
 //-----------------------------------
 //
 // DOJO.Link - Link webGL to OpenSeaDragon
-// Calls DOJO.Input + openSeadragonGL
+// Calls DOJO.Input
 // -- Called by main.js
 //-----------------------------------
 
@@ -10,50 +10,16 @@ DOJO.Link = function(scope) {
     this.input = new DOJO.Input(scope);
     window.addEventListener('keyup',this.input.key.bind(this.input));
     this.buttons = ['up','down'].map(this.linkButtons,this);
-    this.openSD = scope.openSD;
 }
 DOJO.Link.prototype = {
 
     init: function(){
-        // Make a link to webGL
-        var seaGL = new openSeadragonGL(this.openSD);
-        seaGL.fShader = 'shaders/fragment/outline.glsl';
-        seaGL.vShader = 'shaders/vertex/square.glsl';
-
-        // Add WebGL Drawing and Layer Buttons
-        this.buttons.map(seaGL.button, this.input);
-//        seaGL.addHandler('tile-drawing',this.drawing.bind(seaGL,this.unzip));
-        this.openSD.addHandler('update-level',this.input.leveler.bind(this.input));
-        seaGL.init();
+        this.buttons.map(openSeadragonGL.prototype.button, this.input);
     },
     linkButtons: function(name) {
         return {
             name: name,
             onClick: this.input.waiter.bind(this.input,name)
         }
-    },
-    drawing: function(unzip, callback, e) {
-        if ('drawn' in e.tile) {
-            return;
-        }
-        if(e.tiledImage.source.segmentation){
-            callback = callback.bind(this,e);
-            unzip.call(this.viaGL, callback, e);
-        };
-        e.tile.drawn = 1;
-    },
-    unzip: function(callback, e){
-
-        var buffer = function(given){
-             given.xhr.responseType = 'arraybuffer';
-             given.url = e.tile.url;
-             return 0;
-        }
-        var unzip = function(blob){
-            var compressed = new Zlib.Inflate(new Uint8Array(blob));
-            return compressed.decompress();
-        }
-
-        return this.getter.call(buffer).then(unzip).then(callback);
     }
 }
